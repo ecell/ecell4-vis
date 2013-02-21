0598bfcef9a1007af0390f3c1131465fa9c465ac
Yasushi Masuda <whosaysni@gmail.com>
2013-02-21 18:46:24 +0900

    Built-in specs are now defined in pipeline/specs.

---
+++ ec4vis/pipeline/specs.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+"""ec4vis.pipeline.specs --- Built-in DataSpec definitions.
+"""
+# this allows module-wise execution
+try:
+    import ec4vis
+except ImportError:
+    import sys, os
+    p = os.path.abspath(__file__); sys.path.insert(0, p[:p.rindex(os.sep+'ec4vis')])
+
+from ec4vis.pipeline import PipelineSpec
+
+
+class Hdf5DataSpec(PipelineSpec):
+    pass
+
+class NumberOfItemsSpec(PipelineSpec):
+    pass
+
+
