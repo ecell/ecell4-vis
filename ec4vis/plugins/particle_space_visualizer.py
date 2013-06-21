93467aca857f81f48aae04e1b2d0b700a8f4b58f
Kazunari Kaizu <kwaizu@gmail.com>
2013-06-21 19:32:33 +0900

    adapt particle_space_visualizer to base-class changes

--- ec4vis/plugins/particle_space_visualizer.py
+++ ec4vis/plugins/particle_space_visualizer.py
@@ -20,6 +20,7 @@ from ec4vis.pipeline.specs import Hdf5DataSpec, NumberOfItemsSpec
 
 from ec4vis.plugins.particle_csv_loader import ParticleSpaceSpec
 from ec4vis.visualizer.page import register_visualizer_page
+from ec4vis.visualizer.vtk3d import Vtk3dVisualizerNode
 from ec4vis.visualizer.vtk3d.page import Vtk3dVisualizerPage
 from ec4vis.visualizer.vtk3d.visual import ActorsVisual
 
@@ -63,52 +64,7 @@ def get_new_color(idx):
     # else:
     return (1, 1, 1)
 
-class ParticleSpaceVisualizerNode(PipelineNode):
-    """
-    """
-    INPUT_SPEC = [ParticleSpaceSpec, NumberOfItemsSpec]
-    OUTPUT_SPEC = []
-
-    def __init__(self, *args, **kwargs):
-        """Initializer.
-        """
-        # self._simple_visuals = []
-        PipelineNode.__init__(self, *args, **kwargs)
-
-        self.view_scale = 1e-6
-        self.sid_color_map = None
-
-    @log_call
-    def fetch_particle_space(self, **kwargs):
-        ps = self.parent.request_data(ParticleSpaceSpec, **kwargs)
-        if ps is not None:
-            self.update_list(**kwargs)
-        return ps
-
-    @log_call
-    def internal_update(self):
-        """Reset cached particles
-        """
-        # self.view_scale = 1e-6
-        # self.sid_color_map = None
-        pass
-
-    @log_call
-    def update_list(self, **kwargs):
-        ps = self.parent.request_data(ParticleSpaceSpec, **kwargs)
-        if ps is None:
-            return
-
-        if self.sid_color_map is None:
-            self.sid_color_map = {}
-
-        for sp in ps.species:
-            if sp not in self.sid_color_map.keys():
-                self.sid_color_map[sp] = get_new_color(len(self.sid_color_map))
-
-register_pipeline_node(ParticleSpaceVisualizerNode)
-
-class SimpleVisual(ActorsVisual):
+class ParticlesVisual(ActorsVisual):
 
     def __init__(self, *args, **kwargs):
         ActorsVisual.__init__(self, *args, **kwargs)
@@ -120,6 +76,8 @@ class SimpleVisual(ActorsVisual):
         self._actors_cache = {}
 
     def _get_actors(self):
+        """override a base-class member function
+        """
         for sid, actor in self._actors_cache.items():
             self._renderer.RemoveActor(actor)
         self._actors_cache.clear()
@@ -154,8 +112,12 @@ class SimpleVisual(ActorsVisual):
 
                 poly_data = vtk.vtkPolyData()
                 poly_data.SetPoints(points)
-                source = vtk.vtkPointSource()
+
+                # source = vtk.vtkPointSource()
+                # source.SetRadius(radius)
+                source = vtk.vtkSphereSource()
                 source.SetRadius(radius)
+
                 mapper = vtk.vtkGlyph3DMapper()
                 mapper.SetSourceConnection(source.GetOutputPort())
                 mapper.SetInputConnection(poly_data.GetProducerPort())
@@ -164,38 +126,82 @@ class SimpleVisual(ActorsVisual):
                 actor.GetProperty().SetColor(color)
                 self._actors_cache[sid] = actor
 
-            self._renderer.ResetCamera(bounds)
-            if self._axes is not None:
-                self._renderer.RemoveViewProp(self._axes)
-            self._axes = create_axes(bounds)
-            self._axes.SetCamera(self._renderer.GetActiveCamera())
-            self._renderer.AddViewProp(self._axes)
+            # self._renderer.ResetCamera(bounds)
+            # if self._axes is not None:
+            #     self._renderer.RemoveViewProp(self._axes)
+            # self._axes = create_axes(bounds)
+            # self._axes.SetCamera(self._renderer.GetActiveCamera())
+            # self._renderer.AddViewProp(self._axes)
 
         debug('actors: %s' % self._actors_cache)
         return self._actors_cache
 
-    def update(self, data):
+    def reset_actors(self, data):
+        for actor in self._actors_cache.values():
+            self._renderer.RemoveActor(actor)
+        self._actors_cache = {}
+
         self.particle_space = data['particle_space']
         self.view_scale = data['view_scale']
         self.color_map = data['color_map']
 
-class ParticleSpaceVisualizer(Vtk3dVisualizerPage):
+class ParticleSpaceVisualizerNode(Vtk3dVisualizerNode):
+    """
+    """
+    INPUT_SPEC = [ParticleSpaceSpec]
+    OUTPUT_SPEC = []
 
     def __init__(self, *args, **kwargs):
-        Vtk3dVisualizerPage.__init__(self, *args, **kwargs)
+        """Initializer.
+        """
+        Vtk3dVisualizerNode.__init__(self, *args, **kwargs)
+        self.renderer.GetActiveCamera().ParallelProjectionOn()
+        self.particles_visual = ParticlesVisual(
+            renderer=self.renderer, name='particles')
 
-        self.simple_visual = SimpleVisual(renderer=self.renderer, name='simple')
+        self.view_scale = 1e-6
+        self.sid_color_map = None
+
+    @log_call
+    def internal_update(self):
+        """Reset cached particles
+        """
+        # self.view_scale = 1e-6
+        # self.sid_color_map = None
+
+        ps = self.fetch_particle_space()
+        self.particles_visual.reset_actors(
+            dict(particle_space = ps,
+                 view_scale = self.view_scale,
+                 color_map = self.sid_color_map))
+        self.particles_visual.enable()
+
+    @log_call
+    def fetch_particle_space(self, **kwargs):
+        ps = self.parent.request_data(ParticleSpaceSpec, **kwargs)
+        if ps is not None:
+            self.update_list(**kwargs)
+        return ps
+
+    @log_call
+    def update_list(self, **kwargs):
+        ps = self.parent.request_data(ParticleSpaceSpec, **kwargs)
+        if ps is None:
+            return
+
+        if self.sid_color_map is None:
+            self.sid_color_map = {}
+
+        for sp in ps.species:
+            if sp not in self.sid_color_map.keys():
+                self.sid_color_map[sp] = get_new_color(len(self.sid_color_map))
+
+register_pipeline_node(ParticleSpaceVisualizerNode)
+
+class ParticleSpaceVisualizer(Vtk3dVisualizerPage):
 
     @log_call
     def update(self):
-        ps = self.target.fetch_particle_space()
-        self.target.update_list()
-
-        self.simple_visual.update(
-            dict(particle_space = ps,
-                 view_scale = self.target.view_scale,
-                 color_map = self.target.sid_color_map))
-        self.simple_visual.enable()
         self.render()
 
 register_visualizer_page('ParticleSpaceVisualizerNode', ParticleSpaceVisualizer)
@@ -209,14 +215,6 @@ class ParticleSpaceVisualizerInspector(InspectorPage):
         InspectorPage.__init__(self, *args, **kwargs)
 
         widgets = []
-
-        self.index_entry = wx.TextCtrl(
-            self, wx.ID_ANY, "0", style=wx.TE_PROCESS_ENTER)
-        self.max_index_text = wx.StaticText(self, -1, 'Index (0)')
-        # self.max_num_entry.Bind(wx.EVT_TEXT_ENTER, self.max_num_entry_updated)
-        widgets.extend([
-            (self.max_index_text, 0, wx.ALL | wx.EXPAND),
-            (self.index_entry, 1, wx.ALL | wx.EXPAND)])
 
         self.view_scale_entry = wx.TextCtrl(
             self, wx.ID_ANY, "1e-6", style=wx.TE_PROCESS_ENTER)
@@ -240,12 +238,12 @@ class ParticleSpaceVisualizerInspector(InspectorPage):
             (self.listbox, 1, wx.ALL | wx.EXPAND)])
 
         self.listbox.Bind(wx.EVT_RIGHT_DOWN, self.listbox_right_down)
-        
+
         self.button = wx.Button(self, wx.ID_ANY, "save png")
         widgets.extend([
                         (self.button, 1, wx.ALL | wx.EXPAND)])
         self.button.Bind(wx.EVT_BUTTON, self.button_click)
-        
+
         # pack in FlexGridSizer.
         fx_sizer = wx.FlexGridSizer(cols=2, vgap=9, hgap=25)
         fx_sizer.AddMany(widgets)
@@ -312,29 +310,24 @@ class ParticleSpaceVisualizerInspector(InspectorPage):
             for child in self.target.children:
                 child.propagate_down(UpdateEvent(None))
         dlg.Destroy()
-        
+
     @log_call
     def button_click(self, event):
         w2if = vtk.vtkWindowToImageFilter()
         import pdb; pdb.set_trace()
-        w2if.SetInput(self.simple_visual.render)
+        w2if.SetInput(self.particles_visual.render)
         w2if.Update()
-          
+
         writer = vtk.vtkPNGWriter()
         writer.SetFileName("screenshot.png")
         writer.SetInput(w2if.GetOutput())
         writer.Write()
         print "hogehoge"
 
-    @log_call
+    #@log_call
     def update(self):
         """Update UI.
         """
-        max_num = self.target.parent.request_data(NumberOfItemsSpec)
-        self.max_index_text.SetLabel('Index (%d)' % max_num)
-
-        self.target.update_list()
-
         if self.target.sid_color_map is not None:
             sp_list = self.target.sid_color_map.keys()
             sp_list.sort()
