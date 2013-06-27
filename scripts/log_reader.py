92e0d3c6338db5670b0b8e4f735995626e43cda1
Suguru Kato <salmon.bioinformatics@gmail.com>
2013-06-27 15:04:56 +0900

    renamed readLog.py into log_reader.py and added a test file

--- scripts/log_reader.py
+++ scripts/log_reader.py
@@ -1,4 +1,6 @@
-# -*- encoding: utf-8 -*-
+#!/usr/bin/env python
+# coding: utf-8
+
 import struct
 
 class SpatiocyteLogReader:
@@ -347,36 +349,3 @@ class SpatiocyteLogReader:
         size = struct.unpack('i', self.logfile.read(4))[0]
         self.logfile.seek(24*size)
 
-
-def main():
-    '''
-    main function
-    '''
-    name = 'VisualLog.dat'
-    reader = SpatiocyteLogReader(name)
-
-    header = reader.header
-    print '[header]\t',header
-
-    latticeSpecies = reader.latticeSpecies
-    print '[latticeSpecies]\t',latticeSpecies
-
-    polymerSpecies = reader.polymerSpecies
-    print '[polymerSpecies]\t',polymerSpecies
-
-    compVacant = reader.compVacant
-    print '[compVacant]',compVacant
-
-    while not reader.isEnd() :
-        reader.skipSpecies()
-        #species = reader.readSpecies()
-        #print '[species]',species
-
-    print 'CurrentSeek : ',reader.tell(),'\n'
-    print 'Index : 30'
-    print reader.skipSpeciesTo(30)
-
-    reader.closeLogFile()
-
-if __name__ == "__main__":
-    main()
